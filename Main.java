222baf7f875a05a157edbef7cb5880085e7ec690
JakubKwiatek132 <126687451+JakubKwiatek132@users.noreply.github.com>
2023-03-15 13:14:03 +0000

    0 zamienione na 1

--- Main.java
+++ Main.java
@@ -1,7 +1,7 @@
 class Main {
   public static void main(String[] args) {
     for (int i = 0; i <= 100; i++) {
-      if (i % 2 == 0) {
+      if (i % 2 == 1) {
         System.out.println(i);
       }
     }
